99a87b3bfaf6e360ac92c027c542f7e409b51a4a
Ji3hnu <asjishnu320@gmail.com>
2026-01-24 17:18:21 +0530

    Count Increase

---
+++ Assets/scripts/HitCube.cs
@@ -0,0 +1,54 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class HitCube :NetworkBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public NetworkVariable<int> m_HitCount = new NetworkVariable<int>(0);
+    public NetworkVariable<int> m_HostCount = new NetworkVariable<int>(0);
+    public NetworkVariable<int> m_ClientCount = new NetworkVariable<int>(0);
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        m_HitCount.OnValueChanged += OnHitValueChanged;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        base.OnNetworkDespawn();
+        m_HitCount.OnValueChanged -= OnHitValueChanged;
+    }
+
+    private void OnHitValueChanged(int oldValue, int newValue)
+    {
+        Debug.Log("Hit Count Updated : " + newValue);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Detect")
+        {
+            Debug.Log("hit");
+            NetworkObject networkObject = other.GetComponent<NetworkObject>();
+            if (IsClient && networkObject != null && networkObject.IsOwner)
+            {
+                AddHitServerRpc(networkObject.OwnerClientId);
+            }
+        }
+    }
+
+    [Rpc(SendTo.Server)]
+    private void AddHitServerRpc(ulong playerId)
+    {
+        m_HitCount.Value++;
+        if (playerId == 0)
+        {
+            m_HostCount.Value++;
+        }
+        else if (playerId == 1)
+        {
+            m_ClientCount.Value++;
+        }
+    }
+}
