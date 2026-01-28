d662a8676ba8babb6f33da826fe1be33e8110de1
anandakrishnangit <anandakrishnankp6@gmail.com>
2026-01-28 12:11:33 +0530

    car spawn fixed

--- Assets/scripts/MultiplayerSpwn.cs
+++ Assets/scripts/MultiplayerSpwn.cs
@@ -1,21 +1,53 @@
+using System.Collections;
 using Unity.Netcode;
 using UnityEngine;
 [DefaultExecutionOrder(0)]
 
 public class MultiplayerSpwn : NetworkBehaviour
 {
+    Rigidbody rb;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
 
+        if (IsServer)
+        {
+            StartCoroutine(Spawn());
+
+        }
+        StartCoroutine(Onphysics());
+
+
+    }
+    IEnumerator Spawn()
+    {
 
         if (OwnerClientId == 0)
-        {
             transform.position = GameObject.FindGameObjectWithTag("SpawnHost").transform.position;
-        }
+
         if (OwnerClientId == 1)
-        {
             transform.position = GameObject.FindGameObjectWithTag("SpawnClient").transform.position;
-        }
+
+
+        yield return new WaitForSeconds(5f);
+
+
+        rb.isKinematic = false;
+
+
+    }
+
+
+
+    IEnumerator Onphysics()
+    {
+        yield return new WaitForSeconds(5f);
+
+        rb.isKinematic = false;
     }
 }
